46d94dc8210e91194df0540f6f2a86c31d5b8a77
Mateus Pinto da Silva <mateus.p.silva@ufv.br>
2021-10-07 12:51:33 -0300

    new symbol table

--- src/symbol_table.h
+++ src/symbol_table.h
@@ -6,82 +6,83 @@
 #include <string.h>
 
 #define SYMBOL_TABLE_SIZE 1024
+#define IDENTIFIER_ALLOC_SIZE 64
 
-typedef struct {
-  char *identifier;
+typedef struct
+{
+  char identifier[IDENTIFIER_ALLOC_SIZE];
 } SymbolTableEntry;
 
-int SymbolTableEntryNew(SymbolTableEntry *symbolTableEntry) {
-  symbolTableEntry->identifier = NULL;
+int SymbolTableEntryNew(SymbolTableEntry *symbolTableEntry)
+{
+  strcpy(symbolTableEntry->identifier, "");
   return 0;
 }
 
-int SymbolTableEntryFill(SymbolTableEntry *symbolTableEntry, char *identifier) {
-  symbolTableEntry->identifier =
-      (char *)malloc(strlen(identifier) * sizeof(char));
-
-  if (symbolTableEntry->identifier != NULL) {
-    strcpy(symbolTableEntry->identifier, identifier);
-    return 0;
-  } else {
-    return 1;
-  }
+int SymbolTableIsEmpty(SymbolTableEntry *symbolTableEntry)
+{
+  return strcmp(symbolTableEntry->identifier, "") == 0 ? 1 : 0;
 }
 
-int SymbolTableEntryTest(SymbolTableEntry *symbolTableEntry, char *identifier) {
+int SymbolTableEntryFill(SymbolTableEntry *symbolTableEntry, char *identifier)
+{
+  strcpy(symbolTableEntry->identifier, identifier);
+  return 0;
+}
+
+int SymbolTableEntryTest(SymbolTableEntry *symbolTableEntry, char *identifier)
+{
   return strcmp(symbolTableEntry->identifier, identifier) == 0 ? 1 : 0;
 }
 
-int SymbolTableEntryPrint(SymbolTableEntry *symbolTableEntry) {
+int SymbolTableEntryPrint(SymbolTableEntry *symbolTableEntry)
+{
   return printf("%s\n", symbolTableEntry->identifier);
 }
 
-typedef struct {
+typedef struct
+{
   SymbolTableEntry table[SYMBOL_TABLE_SIZE];
+  size_t next_empty_slot;
 } SymbolTable;
 
-int SymbolTableNew(SymbolTable *symbolTable) {
-  for (size_t i = 0; i < SYMBOL_TABLE_SIZE; i++) {
+int SymbolTableNew(SymbolTable *symbolTable)
+{
+  for (size_t i = 0; i < SYMBOL_TABLE_SIZE; i++)
+  {
     SymbolTableEntryNew(&(symbolTable->table[i]));
   }
 
+  symbolTable->next_empty_slot = 0;
   return 0;
 }
 
-int SymbolTablePrint(SymbolTable *symbolTable) {
-  printf("\nSymbol Table:\n");
-
-  for (size_t i = 0; i < SYMBOL_TABLE_SIZE; i++) {
-    if (symbolTable->table[i].identifier != NULL) {
-      SymbolTableEntryPrint(&(symbolTable->table[i]));
-    }
+int SymbolTablePrint(SymbolTable *symbolTable)
+{
+  printf("Symbol Table: <identifier>\n");
+  for (size_t i = 0; i < symbolTable->next_empty_slot; i++)
+  {
+    SymbolTableEntryPrint(&(symbolTable->table[i]));
   }
-
   return 0;
 }
 
-int SymbolTableNewEntry(SymbolTable *symbolTable, char *identifier) {
-  for (size_t i = 0; i < SYMBOL_TABLE_SIZE; i++) {
-    if (symbolTable->table[i].identifier == NULL) {
-      SymbolTableEntryFill(&(symbolTable->table[i]), identifier);
-      return 0;
-    }
-  }
-
-  return 1;
+int SymbolTableAddEntry(SymbolTable *symbolTable, char *identifier)
+{
+  SymbolTableEntryFill(&(symbolTable->table[symbolTable->next_empty_slot]), identifier);
+  symbolTable->next_empty_slot++;
+  return 0;
 }
 
-int SymbolTableEntryExists(SymbolTable *symbolTable, char *identifier) {
-  for (size_t i = 0; i < SYMBOL_TABLE_SIZE; i++) {
-    if (symbolTable->table[i].identifier == NULL) {
-      return 0;
-    }
-
-    if (SymbolTableEntryTest(&(symbolTable->table[i]), identifier)) {
+int SymbolTableExistEntry(SymbolTable *symbolTable, char *identifier)
+{
+  for (size_t i = 0; i < symbolTable->next_empty_slot; i++)
+  {
+    if (SymbolTableEntryTest(&(symbolTable->table[i]), identifier))
+    {
       return 1;
     }
   }
-
   return 0;
 }
 
