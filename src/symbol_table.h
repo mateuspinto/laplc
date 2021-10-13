1667aa92cfca753e6d3c68d92d5778aaa9af6cae
Mateus Pinto da Silva <mateus.p.silva@ufv.br>
2021-10-13 11:48:16 -0300

    iniciada entrega 4

--- src/symbol_table.h
+++ src/symbol_table.h
@@ -7,81 +7,250 @@
 
 #define SYMBOL_TABLE_SIZE 1024
 #define IDENTIFIER_ALLOC_SIZE 64
+#define MAX_ARGUMENT_NUMBER 32
+
+typedef enum
+{
+  STD_UNDEFINED,
+  STD_LET,
+  STD_FUNCTION,
+  STD_CONST
+} SymbolTableDefinition;
+
+int SymbolTableDefinitionPrintf(SymbolTableDefinition symbolTableDefinition)
+{
+  switch (symbolTableDefinition)
+  {
+  case STD_LET:
+    printf("Let");
+    break;
+
+  case STD_CONST:
+    printf("Const");
+    break;
+
+  case STD_FUNCTION:
+    printf("Function");
+    break;
+
+  default:
+    break;
+  }
+}
+
+typedef enum
+{
+  STT_UNDEFINED,
+  STT_INT,
+  STT_FLOAT,
+  STT_CHAR,
+  STT_BOOL
+} SymbolTableType;
+
+int SymbolTableTypePrintf(SymbolTableType symbolTableType)
+{
+  switch (symbolTableType)
+  {
+  case STT_INT:
+    printf("Int");
+    break;
+
+  case STT_FLOAT:
+    printf("Flot");
+    break;
+
+  case STT_CHAR:
+    printf("Char");
+    break;
+
+  case STT_BOOL:
+    printf("Bool");
+    break;
+
+  default:
+    break;
+  }
+}
 
 typedef struct
 {
   char identifier[IDENTIFIER_ALLOC_SIZE];
+
+  SymbolTableDefinition definiton;
+  SymbolTableType returnType;
+  SymbolTableType argumentTypes[MAX_ARGUMENT_NUMBER];
+
+  int argumentNumber;
+
 } SymbolTableEntry;
 
-int SymbolTableEntryNew(SymbolTableEntry *symbolTableEntry)
+int SymbolTableEntryInitialize(SymbolTableEntry *symbolTableEntry)
 {
   strcpy(symbolTableEntry->identifier, "");
+
+  symbolTableEntry->definiton = STD_UNDEFINED;
+  symbolTableEntry->returnType = STT_UNDEFINED;
+  symbolTableEntry->argumentNumber = 0;
+
+  for (size_t i = 0; i < MAX_ARGUMENT_NUMBER; i++)
+  {
+    symbolTableEntry->argumentTypes[i] = STT_UNDEFINED;
+  }
+
   return 0;
 }
 
-int SymbolTableIsEmpty(SymbolTableEntry *symbolTableEntry)
-{
-  return strcmp(symbolTableEntry->identifier, "") == 0 ? 1 : 0;
-}
-
-int SymbolTableEntryFill(SymbolTableEntry *symbolTableEntry, char *identifier)
+int SymbolTableEntrySetLet(SymbolTableEntry *symbolTableEntry, char *identifier, SymbolTableType type)
 {
   strcpy(symbolTableEntry->identifier, identifier);
+
+  symbolTableEntry->returnType = type;
+  symbolTableEntry->argumentNumber = 0;
+  symbolTableEntry->definiton = STD_LET;
+
   return 0;
 }
 
-int SymbolTableEntryTest(SymbolTableEntry *symbolTableEntry, char *identifier)
+int SymbolTableEntrySetConst(SymbolTableEntry *symbolTableEntry, char *identifier, SymbolTableType type)
 {
-  return strcmp(symbolTableEntry->identifier, identifier) == 0 ? 1 : 0;
+  strcpy(symbolTableEntry->identifier, identifier);
+
+  symbolTableEntry->returnType = type;
+  symbolTableEntry->argumentNumber = 0;
+  symbolTableEntry->definiton = STD_CONST;
+
+  return 0;
 }
 
-int SymbolTableEntryPrint(SymbolTableEntry *symbolTableEntry)
+int SymbolTableEntrySetFunction(SymbolTableEntry *symbolTableEntry, char *identifier, SymbolTableType returnType, SymbolTableType *argumentTypes)
 {
-  return printf("%s\n", symbolTableEntry->identifier);
+  int argumentNumber = 0;
+
+  strcpy(symbolTableEntry->identifier, identifier);
+
+  symbolTableEntry->returnType = returnType;
+  symbolTableEntry->definiton = STD_FUNCTION;
+
+  for (size_t i = 0; i < MAX_ARGUMENT_NUMBER; i++)
+  {
+    if (argumentTypes[i] != STT_UNDEFINED)
+      argumentNumber++;
+
+    symbolTableEntry->argumentTypes[i] = argumentTypes[i];
+  }
+
+  symbolTableEntry->argumentNumber = argumentNumber;
+
+  return 0;
+}
+
+int SymbolTableEntryPrintf(SymbolTableEntry *symbolTableEntry)
+{
+  printf("%s - ", symbolTableEntry->identifier);
+  SymbolTableDefinitionPrintf(symbolTableEntry->definiton);
+  printf(" - ");
+  SymbolTableTypePrintf(symbolTableEntry->returnType);
+
+  printf(" - [");
+  for (size_t i = 0; i < symbolTableEntry->argumentNumber; i++)
+  {
+    SymbolTableTypePrintf(symbolTableEntry->argumentTypes[i]);
+
+    if (i != symbolTableEntry->argumentNumber - 1)
+      printf(", ");
+  }
+  printf("]");
+
+  printf("\n");
+  return 0;
 }
 
 typedef struct
 {
   SymbolTableEntry table[SYMBOL_TABLE_SIZE];
+
   size_t next_empty_slot;
 } SymbolTable;
 
-int SymbolTableNew(SymbolTable *symbolTable)
+int SymbolTableInitialize(SymbolTable *symbolTable)
 {
   for (size_t i = 0; i < SYMBOL_TABLE_SIZE; i++)
   {
-    SymbolTableEntryNew(&(symbolTable->table[i]));
+    SymbolTableEntryInitialize(&(symbolTable->table[i]));
   }
 
   symbolTable->next_empty_slot = 0;
   return 0;
 }
 
-int SymbolTablePrint(SymbolTable *symbolTable)
+int SymbolTableAddLet(SymbolTable *symbolTable, char *identifier, SymbolTableType type)
 {
-  printf("Symbol Table: <identifier>\n");
-  for (size_t i = 0; i < symbolTable->next_empty_slot; i++)
+  if (symbolTable->next_empty_slot == SYMBOL_TABLE_SIZE)
   {
-    SymbolTableEntryPrint(&(symbolTable->table[i]));
+    printf("laplc: symbol table full");
+    exit(1);
   }
-  return 0;
-}
 
-int SymbolTableAddEntry(SymbolTable *symbolTable, char *identifier)
-{
-  SymbolTableEntryFill(&(symbolTable->table[symbolTable->next_empty_slot]), identifier);
+  SymbolTableEntrySetLet(symbolTable->table + symbolTable->next_empty_slot, identifier, type);
   symbolTable->next_empty_slot++;
-  return 0;
 }
 
-int SymbolTableExistEntry(SymbolTable *symbolTable, char *identifier)
+int SymbolTableAddConst(SymbolTable *symbolTable, char *identifier, SymbolTableType type)
 {
+  if (symbolTable->next_empty_slot == SYMBOL_TABLE_SIZE)
+  {
+    printf("laplc: memory error, symbol table full");
+    exit(1);
+  }
+
+  SymbolTableEntrySetConst(symbolTable->table + symbolTable->next_empty_slot, identifier, type);
+  symbolTable->next_empty_slot++;
+}
+
+int SymbolTableAddFunction(SymbolTable *symbolTable, char *identifier, SymbolTableType returnType, SymbolTableType *argumentTypes)
+{
+  if (symbolTable->next_empty_slot == SYMBOL_TABLE_SIZE)
+  {
+    printf("laplc: memory error, symbol table full");
+    exit(1);
+  }
+
+  SymbolTableEntrySetFunction(symbolTable->table + symbolTable->next_empty_slot, identifier, returnType, argumentTypes);
+  symbolTable->next_empty_slot++;
+}
+
+SymbolTableType SymbolTableGetVariableOrConstType(SymbolTable *symbolTable, char *identifier)
+{
+  for (size_t i = 0; i < SYMBOL_TABLE_SIZE; i++)
+  {
+    if ((strcmp((symbolTable->table + i)->identifier, identifier) == 0 ? 1 : 0) && (((symbolTable->table + i)->definiton == STD_LET) || ((symbolTable->table + i)->definiton == STD_CONST)))
+    {
+      return (symbolTable->table + i)->returnType;
+    }
+  }
+
+  return STT_UNDEFINED;
+}
+
+SymbolTableType SymbolTableGetFunctionType(SymbolTable *symbolTable, char *identifier)
+{
+  for (size_t i = 0; i < SYMBOL_TABLE_SIZE; i++)
+  {
+    if ((strcmp((symbolTable->table + i)->identifier, identifier) == 0 ? 1 : 0) && ((symbolTable->table + i)->definiton == STD_FUNCTION))
+    {
+      return (symbolTable->table + i)->returnType;
+    }
+  }
+
+  return STT_UNDEFINED;
+}
+
+int SymbolTablePrintf(SymbolTable *symbolTable)
+{
+  printf("Symbol Table: <Identifier>, <Definition>, <ReturnType>, [<ArgumentTypes>*]\n");
   for (size_t i = 0; i < symbolTable->next_empty_slot; i++)
   {
-    if (SymbolTableEntryTest(&(symbolTable->table[i]), identifier))
-    {
-      return 1;
-    }
+    SymbolTableEntryPrintf(&(symbolTable->table[i]));
   }
   return 0;
 }
--- src/syntax.y
+++ src/syntax.y
@@ -1,37 +1,29 @@
 %{
-#define YYSTYPE char*
+#define YYSTYPE char *
 #include <stdio.h>
 #include <stdlib.h>
-#include "symbol_table.h"
 
 int yydebug = 1;
 
 extern int yylex();
 extern int yylineno;
 
-void yyerror(const char* s) {
+void yyerror(const char* s) 
+{
 	fprintf(stderr, "\nlaplc error: %s on line %d\n", s, yylineno);
 	exit(1);
 }
 
-void deerror(const char* s) {
+void deerror(const char* s) 
+{
 	fprintf(stderr, "\nlaplc error: Identifier %s on line %d does not exist\n", s, yylineno);
 	exit(1);
 }
-
-SymbolTable symbolTable;
 %}
 
 %token LET
 %token CONST
 %token FUNCTION
-%token IMPORT
-%token AS
-%token FROM
-%token MATCH
-%token DATA
-%token DEFAULT
-%token TYPE_STRUCT
 %token TYPE_INT
 %token TYPE_FLOAT
 %token TYPE_CHAR
@@ -41,8 +33,7 @@ SymbolTable symbolTable;
 %token VALUE_BOOL
 %token VALUE_CHAR
 %token VALUE_STRING
-%token LOWERCASE_IDENTIFIER
-%token UPPERCASE_IDENTIFIER
+%token IDENTIFIER
 %token NOT
 %token PLUS
 %token MINUS
@@ -75,26 +66,18 @@ SymbolTable symbolTable;
 %%
 
 run: 
-	| list run
+	| statements run
 ;
 
-list: variable_definition
+statements: variable_definition
 	| const_definition
 	| function_declaration
-	| data_declariation
-	| function_import
-;
-
-vec_type: OPEN_BRACKET type_specifier SEMICOLON VALUE_INT CLOSE_BRACKET
 ;
 
 type_specifier: TYPE_INT
 	| TYPE_FLOAT
  	| TYPE_CHAR
-	| TYPE_BOOL 
-	| TYPE_STRUCT
-	| vec_type
-	| UPPERCASE_IDENTIFIER {free($1);}
+	| TYPE_BOOL
 ;
 
 operator: NOT
@@ -113,156 +96,57 @@ operator: NOT
 	| OR
 ;
 
-aux_value_vec: value
-	| value COMMA aux_value_vec
-;
-
-value_vec: OPEN_BRACKET aux_value_vec CLOSE_BRACKET
-;
-
-aux_struct_value: LOWERCASE_IDENTIFIER COLON value {free($1);}
-	| LOWERCASE_IDENTIFIER COLON value COMMA aux_struct_value {free($1);}
-;
-
-struct_value: OPEN_BRACE aux_struct_value CLOSE_BRACE
-;
-
 value: VALUE_INT
 	| VALUE_BOOL
 	| VALUE_CHAR
 	| VALUE_FLOAT
-	| VALUE_STRING
-	| value_vec
-	| struct_value
 ;
 
-parameter: LOWERCASE_IDENTIFIER  {
-				if(!SymbolTableExistEntry(&symbolTable, $1))
-					deerror($1);
-				free($1);
-			}
+element: IDENTIFIER
 	| value
-	| fluent_api
 	| function_call
 ;
 
-expression: parameter
-	| parameter operator expression
-	| OPEN_PARENTHESIS expression CLOSE_PARENTHESIS
+expression: element
+	| element operator expression
 	| OPEN_PARENTHESIS expression CLOSE_PARENTHESIS operator expression
 ;
 
-variable_definition: LET LOWERCASE_IDENTIFIER COLON type_specifier 
-		DEFINITION expression SEMICOLON {
-				if(!SymbolTableExistEntry(&symbolTable, $2))
-					SymbolTableAddEntry(&symbolTable, $2);
-				free($2);
-			}
+variable_definition: LET IDENTIFIER COLON type_specifier 
+		DEFINITION expression SEMICOLON
 ;
 
-const_definition: CONST LOWERCASE_IDENTIFIER COLON type_specifier 
-		DEFINITION expression SEMICOLON {
-				if(!SymbolTableExistEntry(&symbolTable, $2))
-					SymbolTableAddEntry(&symbolTable, $2);
-				free($2);
-			}
+const_definition: CONST IDENTIFIER COLON type_specifier 
+		DEFINITION expression SEMICOLON
 ;
 
-guards: parameter operator parameter COLON OPEN_BRACE parameter CLOSE_BRACE SEMICOLON
-	| parameter operator parameter COLON OPEN_BRACE parameter CLOSE_BRACE SEMICOLON guards
-	| DEFAULT COLON OPEN_BRACE parameter CLOSE_BRACE SEMICOLON
-;
-
-function_definition: OPEN_PARENTHESIS function_definition_parameters CLOSE_PARENTHESIS ARROW MATCH OPEN_BRACE guards CLOSE_BRACE
-	| OPEN_PARENTHESIS function_definition_parameters CLOSE_PARENTHESIS ARROW OPEN_BRACE expression CLOSE_BRACE
+function_definition: OPEN_PARENTHESIS function_definition_parameters CLOSE_PARENTHESIS ARROW OPEN_BRACE expression CLOSE_BRACE
 ;
 
 function_types: type_specifier
 	| type_specifier COMMA function_types
 ;
 
-function_definition_parameters: LOWERCASE_IDENTIFIER {SymbolTableAddEntry(&symbolTable, $1); free($1);}
-	| LOWERCASE_IDENTIFIER COMMA function_definition_parameters {SymbolTableAddEntry(&symbolTable, $1); free($1);}
+function_definition_parameters: IDENTIFIER
+	| IDENTIFIER COMMA function_definition_parameters
 ;
 
-function_call_parameters: parameter
-	| parameter COMMA function_call_parameters
+function_call_parameters: element
+	| element COMMA function_call_parameters
 ;
 
-function_declaration: FUNCTION LOWERCASE_IDENTIFIER COLON OPEN_PARENTHESIS function_types CLOSE_PARENTHESIS
-		type_specifier DEFINITION function_definition SEMICOLON {
-				if(!SymbolTableExistEntry(&symbolTable, $2))
-					SymbolTableAddEntry(&symbolTable, $2);
-				free($2);
-			}
+function_declaration: FUNCTION IDENTIFIER COLON OPEN_PARENTHESIS function_types CLOSE_PARENTHESIS
+		type_specifier DEFINITION function_definition SEMICOLON
 ;
 
-fluent_api: PERIOD LOWERCASE_IDENTIFIER {
-				if(!SymbolTableExistEntry(&symbolTable, $2))
-					deerror($2);
-				free($2);
-			}
-	| LOWERCASE_IDENTIFIER fluent_api {
-				if(!SymbolTableExistEntry(&symbolTable, $1))
-					deerror($1);
-				free($1);
-			}
-	| UPPERCASE_IDENTIFIER fluent_api {
-				if(!SymbolTableExistEntry(&symbolTable, $1))
-					deerror($1);
-				free($1);
-			}
-;
-
-aux_function_call: fluent_api OPEN_PARENTHESIS function_call_parameters CLOSE_PARENTHESIS
-	| LOWERCASE_IDENTIFIER OPEN_PARENTHESIS function_call_parameters CLOSE_PARENTHESIS {
-				if(!SymbolTableExistEntry(&symbolTable, $1))
-					deerror($1);
-				free($1);
-			}
-;
-
-function_call: aux_function_call
-;
-
-aux_struct_definition: LOWERCASE_IDENTIFIER COLON type_specifier {
-				if(!SymbolTableExistEntry(&symbolTable, $1))
-					SymbolTableAddEntry(&symbolTable, $1);
-				free($1);
-					
-			}
-	| LOWERCASE_IDENTIFIER COLON type_specifier COMMA aux_struct_definition {
-				if(!SymbolTableExistEntry(&symbolTable, $1))
-					SymbolTableAddEntry(&symbolTable, $1);
-				free($1);
-			}
-;
-
-struct_definition: OPEN_BRACE aux_struct_definition CLOSE_BRACE
-;
-
-data_declariation: DATA UPPERCASE_IDENTIFIER COLON TYPE_STRUCT DEFINITION struct_definition SEMICOLON {
-				if(!SymbolTableExistEntry(&symbolTable, $2))
-					SymbolTableAddEntry(&symbolTable, $2);
-				free($2);
-			}
-;
-
-function_package: LOWERCASE_IDENTIFIER {SymbolTableAddEntry(&symbolTable, $1); free($1);}
-	| LOWERCASE_IDENTIFIER COMMA function_package {SymbolTableAddEntry(&symbolTable, $1); free($1);}
-;
-
-function_import: FROM UPPERCASE_IDENTIFIER IMPORT function_package {free($2);}
+function_call: IDENTIFIER OPEN_PARENTHESIS function_call_parameters CLOSE_PARENTHESIS
 ;
 
 %%
 
 int main() {
-	SymbolTableNew(&symbolTable);
 	yyparse();
-
 	printf("laplc: well formed program\n\n");
-	SymbolTablePrint(&symbolTable);
 
 	return 0;
 }
